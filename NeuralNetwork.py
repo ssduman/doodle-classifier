fa4fe87c1f8dfb3f53935c9f41962683f57205f7
perought <sduman1818@gmail.com>
2020-02-19 15:12:17 +0300

    assert added

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -26,11 +26,6 @@ class NeuralNetwork(object):
 
         print("layers:", layers)
 
-    def save(self):
-        np.save("weights", self.weights, allow_pickle=True)
-        np.save("biases", self.biases, allow_pickle=True)
-        np.savetxt("names.txt", self.names, delimiter=', ', fmt="%s")
-
     def feedforward(self, a):
         A = a
         for l in range(self.L - 1):
@@ -41,99 +36,10 @@ class NeuralNetwork(object):
 
         return output
 
-    def predict(self, test_data):
-        test_data = np.array(test_data)
-        test_data = test_data.reshape(test_data.shape[0], 1)
-        output = self.feedforward(test_data)
+    def train(self, train_X, train_Y, test_X, test_Y, _mini=256, beta1=0.9, beta2=0.999, epsilon=1e-8, _cost="cross_entropy", optimizer="none"):
+        assert (train_X.shape[0] == self.weights[0].shape[1])  # train_X.shape -> (input_size, m)
+        assert (train_Y.shape[0] == self.layers[-1])
 
-        print("output from nn: {} -> sum: {}".format(output, np.sum(output)))
-        predictions = np.argmax(output)
-        return predictions
-
-    def accuracy(self, test_data, Y):
-        test_results = self.feedforward(test_data).T
-        predict = 0
-        Y = Y.T
-        m = test_data.shape[1]
-        for i in range(m):
-            if np.argmax(test_results[i]) == np.argmax(Y[i]):
-                predict += 1
-        self.precision = (predict / m) * 100
-        self.title += ", acc: %" + str(round(self.precision, 2)) + " (" + str(predict) + "/" + str(m) + ")"
-        self.ax.set(xlabel="iterations", ylabel="cost", title=self.title)
-        plt.savefig("cost.png")
-        # plt.close()   # causes also tkinter to close
-        # plt.show()
-        return self.precision
-
-    def relu(self, x):
-        return np.maximum(x, 0)
-
-    def relu_backward(self, x):
-        return (x > 0).astype(float)
-
-    def softmax(self, data):
-        exps = [np.exp(x) for x in data]
-        sum_of_exps = np.sum(exps)
-        soft = np.array([x / sum_of_exps for x in exps])
-        return soft
-
-    def softmax_backward(self, x):
-        return x * (1. - x)
-
-    def sigmoid(self, z):
-        return 1.0 / (1.0 + np.exp(-z))
-
-    def sigmoid_backward(self, z):
-        return self.sigmoid(z) * (1 - self.sigmoid(z))
-
-    def dropout(self, masks, activation, keep_prob=0.2, layer=0, where="fowardprop"):
-        if where == "fowardprop":
-            mask = np.random.rand(activation.shape[0], activation.shape[1])
-            mask = (mask < keep_prob).astype(int)
-            masks.append(mask)
-            activation *= mask
-            activation /= 0.2
-            return activation
-        elif where == "backprop":
-            activation *= masks[layer]
-            activation /= 0.2
-            return activation
-
-    def L2(self, m, dW=0, lambd=0.2, where="cost"):
-        if where == "cost":
-            l2 = 0
-            for i in range(len(self.weights)):
-                l2 += np.sum(np.square(self.weights[i]))
-            return (lambd / (2 * m)) * l2
-        elif where == "backprop":
-            return (lambd / m) * self.weights[dW]
-
-    def cost(self, costs, test_costs, m, predict, Y, test_X, test_Y, optimizer="none", _cost="cross_entropy"):
-        if _cost == "cross_entropy":
-            test_predict = self.feedforward(test_X)
-            m_t = float(test_X.shape[1])
-            test_cost = (np.sum(test_Y * np.log(test_predict) + (1. - test_Y) * np.log(1. - test_predict))) / -m_t
-            test_costs.append(test_cost)
-
-            cost = (np.sum(Y * np.log(predict) + (1. - Y) * np.log(1. - predict))) / -m
-            if optimizer == "L2":
-                cost += self.L2(m)
-            costs.append(cost)
-
-        elif _cost == "mean_square":
-            test_predict = self.feedforward(test_X)
-            m_t = float(test_X.shape[1])
-            test_cost = (np.sum(np.power(test_Y - test_predict, 2))) / m_t
-            test_costs.append(test_cost)
-
-            cost = (np.sum(np.power(Y - predict, 2))) / m
-            if optimizer == "L2":
-                cost += self.L2(m)
-            costs.append(cost)
-
-    def train(self, train_X, train_Y, test_X, test_Y, _mini=256, beta1=0.9, beta2=0.999, epsilon=1e-8,
-              _cost="cross_entropy", optimizer="none"):
         m = float(train_X.shape[1])
         test_costs = []
         costs = []
@@ -245,6 +151,102 @@ class NeuralNetwork(object):
         self.ax.legend()
         l = "layers: " + self.layers.__str__()
         plt.text(1.02, 0.5, l, rotation=90, ha="left", va="center", transform=self.ax.transAxes)
+
+    def dropout(self, masks, activation, keep_prob=0.2, layer=0, where="fowardprop"):
+        if where == "fowardprop":
+            mask = np.random.rand(activation.shape[0], activation.shape[1])
+            mask = (mask < keep_prob).astype(int)
+            masks.append(mask)
+            activation *= mask
+            activation /= 0.2
+            return activation
+        elif where == "backprop":
+            activation *= masks[layer]
+            activation /= 0.2
+            return activation
+
+    def L2(self, m, dW=0, lambd=0.2, where="cost"):
+        if where == "cost":
+            l2 = 0
+            for i in range(len(self.weights)):
+                l2 += np.sum(np.square(self.weights[i]))
+            return (lambd / (2 * m)) * l2
+        elif where == "backprop":
+            return (lambd / m) * self.weights[dW]
+
+    def predict(self, test_data):
+        test_data = np.array(test_data)
+        test_data = test_data.reshape(test_data.shape[0], 1)
+        output = self.feedforward(test_data)
+
+        print("output from nn: {} -> sum: {}".format(output, np.sum(output)))
+        predictions = np.argmax(output)
+        return predictions
+
+    def accuracy(self, test_data, Y):
+        test_results = self.feedforward(test_data).T
+        predict = 0
+        Y = Y.T
+        m = test_data.shape[1]
+        for i in range(m):
+            if np.argmax(test_results[i]) == np.argmax(Y[i]):
+                predict += 1
+        self.precision = (predict / m) * 100
+        self.title += ", acc: %" + str(round(self.precision, 2)) + " (" + str(predict) + "/" + str(m) + ")"
+        self.ax.set(xlabel="iterations", ylabel="cost", title=self.title)
+        plt.savefig("cost.png")
+        # plt.close()   # causes also tkinter to close
+        # plt.show()
+        return self.precision
+
+    def cost(self, costs, test_costs, m, predict, Y, test_X, test_Y, optimizer="none", _cost="cross_entropy"):
+        if _cost == "cross_entropy":
+            test_predict = self.feedforward(test_X)
+            m_t = float(test_X.shape[1])
+            test_cost = (np.sum(test_Y * np.log(test_predict) + (1. - test_Y) * np.log(1. - test_predict))) / -m_t
+            test_costs.append(test_cost)
+
+            cost = (np.sum(Y * np.log(predict) + (1. - Y) * np.log(1. - predict))) / -m
+            if optimizer == "L2":
+                cost += self.L2(m)
+            costs.append(cost)
+
+        elif _cost == "mean_square":
+            test_predict = self.feedforward(test_X)
+            m_t = float(test_X.shape[1])
+            test_cost = (np.sum(np.power(test_Y - test_predict, 2))) / m_t
+            test_costs.append(test_cost)
+
+            cost = (np.sum(np.power(Y - predict, 2))) / m
+            if optimizer == "L2":
+                cost += self.L2(m)
+            costs.append(cost)
+
+    def relu(self, x):
+        return np.maximum(x, 0)
+
+    def relu_backward(self, x):
+        return (x > 0).astype(float)
+
+    def softmax(self, data):
+        exps = [np.exp(x) for x in data]
+        sum_of_exps = np.sum(exps)
+        soft = np.array([x / sum_of_exps for x in exps])
+        return soft
+
+    def softmax_backward(self, x):
+        return x * (1. - x)
+
+    def sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
+
+    def sigmoid_backward(self, z):
+        return self.sigmoid(z) * (1 - self.sigmoid(z))
+
+    def save(self):
+        np.save("weights", self.weights, allow_pickle=True)
+        np.save("biases", self.biases, allow_pickle=True)
+        np.savetxt("names.txt", self.names, delimiter=', ', fmt="%s")
 
     def tf(self, x_train, y_train, x_test, y_test):
         x_train = x_train.reshape(x_train.shape[0], 28, 28)
